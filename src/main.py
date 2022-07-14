526faa41aa7dfb8abd6e84b8529bf9419e9abf3c
Armand Silviu Gurgu <armandgurgu23@gmail.com>
2022-07-14 19:31:33 -0400

    Started setting up data processing code.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+from calendar import c
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 
 @hydra.main(version_base='1.1', config_path="config", config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
-    print('\n\n Hello Hydra\n')
+    # Explicitly resolve interpolations in config in order to pass
+    # paths from environment variables.
+    OmegaConf.resolve(cfg)
     print(cfg)
     return
 
